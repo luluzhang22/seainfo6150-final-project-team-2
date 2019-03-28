26f33310dd03d6eae4fcfb9134ecef1817bd0ca3
JCheng0509 <Yusi.Cheng.0509@gmail.com>
2019-03-28 11:16:36 -0700

    About, Contact, 404
    About, Contact, 404 created

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Contact = () => (
-  <h1>Contact</h1>
+  <html>
+  <h1>Contact US</h1>
+  <div>
+    <label>First Name</label>
+    <input type="text"id="fname" name="firstname" placeholder="Your first name.."/>
+    <label>Last Name</label>
+    <input type="text"id="lname" name="lastname" placeholder="Your last name.."/>
+    <label>Email</label>
+    <input type="text"id="email" name="email" placeholder="Your email address.."/>
+    <label>Subject</label>
+    <input type="text"id="subj" name="subject" placeholder="Your subject.."/>
+    <textarea id="subject"name="subject"placeholder="Write something.."></textarea>
+    <input type="submit"value="Submit"/>
+  </div>
+  </html>
+  
 );
 
 export default Contact;
--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import error from './error.png';
 
 const NotFound = () => (
-  <h1>404</h1>
+  <html>
+    <h1>#404: These aren't the pages you're looking for.</h1>
+    <img src={error} alt="404"/>
+    <p>These aren't the pages you're looking for. You can go about your business. Move along... move along.</p>
+  </html> 
 );
 
 export default NotFound;
