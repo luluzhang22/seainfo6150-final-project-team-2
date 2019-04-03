8d51e38e1843d4cca767b5c6189c01b1f3af5053
Bharti <ladhani.b@husky.neu.edu>
2019-04-03 09:57:14 -0700

    Adding Products&CategoryPage

--- src/AllProducts/AllProducts.js
+++ src/AllProducts/AllProducts.js
@@ -2,9 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Products from '../Products/Products';
+import styles from '../ProductsList.module.css'
 
 const AllProducts = ({ categories, products }) => (
-  <Products categories={categories} products={products} />
+  <div className={styles.products_module} >
+  <h2 className={styles.products_header}>What's in our inventory</h2>
+  <ul className={styles.products}>
+  <Products className={styles.products_list} categories={categories} products={products} />
+  </ul>
+  </div>
 );
 
 AllProducts.propTypes = {
--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styles from './Products.module.css'
 
 const Products = ({ categories, products }) => {
   return products.map(product => {
     const category = categories[product.categoryId];
+    let price;
+    if(product.sale>0) {
+      price = <div>
+                <span className={styles.strike_price}>${product.price} </span>
+                <span className={styles.final_price}>${product.sale} </span>
+              </div>
+    } else {
+        price = <div>
+                  <span className={styles.final_price}>${product.price} </span>
+                </div>
+    }
     return (
-      <div key={product.id}>
-          <img src={category.img.sm} />
-          <Link to={`/products/${product.categoryId}/${product.id}`}>
+      <li className = {styles.product_card} key={product.id}>
+          <img className = {styles.product_img} src={category.img.sm} />
+          <Link className = {styles.product_title} to={`/products/${product.categoryId}/${product.id}`}>
             {product.title}
           </Link>
-      </div>
+          {price}
+          <button>
+          <Link className={styles.detail_button} to={`/products/${product.categoryId}/${product.id}`}>
+            Detail
+          </Link>
+          </button>
+          <button className={styles.add_to_cart_button}>Add to Cart</button>
+
+      </li>
     );
   })
 };
