8f24f17eecf110939446d3ca8b2a80d593acbc0a
ziyezhu92 <ziyezhu92@gmail.com>
2019-04-04 13:22:39 -0700

    ProductDetails Update

--- src/ProductDetail/ProductDetail.js
+++ src/ProductDetail/ProductDetail.js
@@ -14,26 +14,47 @@ class ProductDetail extends PureComponent {
       selectProductId
     } = this.props;
 
+    let button;
+    let price;
+    if(product.sale>0) {
+      price = <span>
+                <span className={styles.strike_price}>${product.price} </span>
+                <span className={styles.final_price}>${product.sale} </span>
+              </span>
+    } else {
+        price = <span>
+                  <span className={styles.final_price}>${product.price} </span>
+                </span>
+    }
+
+    if(product.available) {
+        button = <div>
+                    {/* start order button */}
+                    <Link to="/order/1" onClick={selectProductId.bind(null, product.id)}>
+                      <button className={styles.Button}>Add to Cart</button>
+                    </Link>
+                    {/* end order button */}
+                </div>
+    } else {
+        button = <div className={styles.sorryMessage}>Sorry, Currently Not Available!</div>
+    }
+
+
     const category = categories[product.categoryId];
     return (
-      <div>
-        <span>{product.title}</span>
-        <img src={category.img.lg} />
-
-        <div>
+      <div className={styles.wholeContainer}>
+        <img className={styles.img} src={category.img.lg} />
+        <div className={styles.imgTitle}>{product.title}</div>
+        <div className={styles.innerContainer}>
             <ul>
-              <li className={styles.ID}>category: {product.categoryId}</li>
-              <li className={styles.year}>year: {product.year}</li>
-              <li className={styles.price}>price: {product.price}</li>
-              <li className={styles.sale}>sale: {product.sale}</li>
-              <li className={styles.title}>title: {product.title}</li>
-              <li className={styles.description}>description: {product.description}</li>
+              <li className={styles.ID}>Category: {product.categoryId}</li>
+              <li className={styles.year}>Year: {product.year}</li>
+              <li className={styles.price}>Price: {price}</li>
+              <li className={styles.title}>Title: {product.title}</li>
+              <li className={styles.description}>Description: {product.description}</li>
             </ul>
+            <div>{button}</div>
         </div>
-        {/* start order button */}
-        <Link to="/order/1" onClick={selectProductId.bind(null, product.id)}>Add to Cart</Link>
-        {/* end order button */}
-
       </div>
     );
   }
