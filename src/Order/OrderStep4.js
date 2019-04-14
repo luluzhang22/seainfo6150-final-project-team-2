a4c7b6fb59c8dca3df4965b615857f52285b630d
ziyezhu92 <ziyezhu92@gmail.com>
2019-04-14 04:38:31 -0700

    orderStep4 update

--- src/Order/OrderStep4.js
+++ src/Order/OrderStep4.js
@@ -30,8 +30,9 @@ class OrderStep4 extends Component {
       } = this.props;
 
       let hoodOrnament;
-      if(selectedOptions.hasHoodOrnament === "Yes"){
-          hoodOrnament = <div>
+      if(selectedOptions.hasHoodOrnament === "true"){
+          hoodOrnament =
+          <div>
           <span>{options.hoodOrnament.name}</span>
           <select id="hoodOrnament-select" onChange={setProductOption.bind(null, 'hoodOrnament')}>
             {
@@ -44,11 +45,11 @@ class OrderStep4 extends Component {
           </select>
           <ul className={styles.unorderedList}>
             {
-              Object.values(options.hoodOrnament.values).map(hoodOrnament => {
+              Object.values(options.hoodOrnament.values).map((hoodOrnament,key) => {
                 return (
-                    <li key={hoodOrnament}>
-                      <p>{hoodOrnament.id}</p>
-                      <img className={styles.order4Img} src={hoodOrnament.img} />
+                    <li className={styles.listItem} key={key}>
+                      <div className={styles.listP}>{hoodOrnament.id}</div>
+                      <img className={styles.order4Img} src={hoodOrnament.img} ult={hoodOrnament.id}/>
                     </li>
                 );
               })
@@ -56,44 +57,76 @@ class OrderStep4 extends Component {
           </ul>
         </div>
       }else{
-          // hoodOrnament = <span>No</span>
+          hoodOrnament = <div></div>
       }
 
       let trunkMonkey;
-      if(selectedOptions.hasTrunkMonkey === 'Yes'){
-        trunkMonkey =                     <div>
-                              <span>{options.trunkMonkey.name}</span>
-                              <select id="trunkMonkey-select" onChange={setProductOption.bind(null, 'trunkMonkey')}>
-                                <option value="" className={styles.blankOption}></option>
-                                {
-                                  Object.values(options.trunkMonkey.values).map(trunkMonkey => {
-                                    return (
-                                        <option value={trunkMonkey.id}>{trunkMonkey.id}</option>
-                                      );
-                                    })
-                                }
-                              </select>
+      if(selectedOptions.hasTrunkMonkey === 'true'){
+        trunkMonkey =<div>
+              <span>{options.trunkMonkey.name}</span>
+                  <select id="trunkMonkey-select" onChange={setProductOption.bind(null, 'trunkMonkey')}>
+                    {
+                        Object.values(options.trunkMonkey.values).map(trunkMonkey => {
+                            return (
+                                <option value={trunkMonkey.id}>{trunkMonkey.id}</option>
+                                );
+                              })
+                            }
+                            </select>
                               <ul className={styles.unorderedList}>
                                 {
-                                  Object.values(options.trunkMonkey.values).map(trunkMonkey => {
+                                  Object.values(options.trunkMonkey.values).map((trunkMonkey,key)=> {
                                     return (
-                                        <li key={trunkMonkey}>
-                                          <p>{trunkMonkey.id}</p>
-                                          <img className={styles.order4Img} src={trunkMonkey.img.sm} />
+                                        <li key={key} className={styles.listItem}>
+                                          <div className={styles.listP}>{trunkMonkey.id}</div>
+                                          <img className={styles.order4Img} src={trunkMonkey.img.sm} alt={trunkMonkey.id}/>
                                         </li>
                                       );
                                     })
                                   }
                                 </ul>
                             </div>
+      }else{
       }
 
       let monogram;
-      if(selectedOptions.hasTrunkMonkey === 'Yes'){
-
+      if(selectedOptions.hasMonogrammedSteeringWheelCover === 'true'){
+        monogram = <div>
+          <span>{options.monogram.name}(Three Letters)</span>
+          <input id="hasMonogrammedSteeringWheelCover" onChange={setProductOption.bind(null, 'monogram')}
+          pattern='[A-z]{3}'/>
+        </div>
       }else{
-
       }
+
+      // const hoodOraType = () => {
+      //   if (selectedOptions.hasHoodOrnament === 'true') {
+      //     return(
+      //       <div className={styles.oras}>
+      //         <h4>{options.hoodOrnament.name}</h4>
+      //         <div className={styles.oraType}>
+      //           {Object.keys(options.hoodOrnament.values).map((value, index) => {
+      //             return (
+      //               <div key={index} className={styles.singleOra}>
+      //                 <img src={options.hoodOrnament.values[value].img} alt={options.hoodOrnament.values[value].id}/>
+      //                 <label>
+      //                   <input
+      //                     type='radio'
+      //                     name='hoodOrnament'
+      //                     value={options.hoodOrnament.values[value].id || ''}
+      //                     onChange={setProductOption.bind(null, 'hoodOrnament')}
+      //                     defaultChecked={value==='battleship'}
+      //                   />
+      //                   {value}
+      //                 </label>
+      //               </div>
+      //               )
+      //           })}
+      //         </div>
+      //       </div>
+      //     )
+      //   }
+      // };
 
       const product = this.props.products[selectedProductId];
       return this.state.submittedSuccessfully
@@ -108,14 +141,14 @@ class OrderStep4 extends Component {
                     <span>{options.hasGPS.name}
                       <select id="hasGPS-select" onChange={setProductOption.bind(null, 'hasGPS')}>
                         <option value="" className={styles.blankOption}></option>
-                        <option value="Yes">Yes</option>
-                        <option value="No">No</option>
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
                       </select>
                     </span>
                     </div>
 
                     <div>
-                      <spam>{options.spareTire.name}
+                      <span>{options.spareTire.name}
                         <select id="spareTire-select" onChange={setProductOption.bind(null, 'spareTire')}>
                           <option value="" className={styles.blankOption}></option>
                           <option value="S">S</option>
@@ -123,42 +156,42 @@ class OrderStep4 extends Component {
                           <option value="L">L</option>
                           <option value="XL">XL</option>
                         </select>
-                      </spam>
+                      </span>
                     </div>
 
+
                     <div>
-                      <span>{options.hasHoodOrnament.name}</span>
+                      <span>{options.hasHoodOrnament.name}($50 extra)</span>
                       <select id="hasHoodOrnament-select" onChange={setProductOption.bind(null, 'hasHoodOrnament')}>
                         <option value="" className={styles.blankOption}></option>
-                        <option value="Yes">Yes</option>
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                      </select>
+                    </div>
+
+
+                    <div>{hoodOrnament}</div>
+
+                    <div>
+                      <span>{options.hasTrunkMonkey.name}($50 extra)</span>
+                      <select id="hasTrunkMonkey-select" onChange={setProductOption.bind(null, 'hasTrunkMonkey')}>
+                        <option value="" className={styles.blankOption}></option>
+                        <option value="true">Yes</option>
                         <option value="No">No</option>
                       </select>
                     </div>
 
-                    <div>{hoodOrnament}</div>
-
-
+                    <div>{trunkMonkey}</div>
 
                     <div>
-                      <span>{options.hasTrunkMonkey.name}</span>
-                      <select id="hasTrunkMonkey-select" onChange={setProductOption.bind(null, 'hasTrunkMonkey')}>
-                        <option value="" className={styles.blankOption}></option>
-                        <option value="yes">Yes</option>
-                        <option value="no">No</option>
-                      </select>
-                    </div>
-
-
-                    <div>
-                      <span>{options.hasMonogrammedSteeringWheelCover.name}</span>
+                      <span>{options.hasMonogrammedSteeringWheelCover.name}($50 extra)</span>
                       <select id="hasMonogrammedSteeringWheelCover" onChange={setProductOption.bind(null, 'hasMonogrammedSteeringWheelCover') }>
                         <option value="" className={styles.blankOption}></option>
-                        <option value="yes">Yes</option>
+                        <option value="true">Yes</option>
                         <option value="no">No</option>
                       </select>
-                      <span>{options.monogram.name}(Three Letters)</span>
-                      <input id="hasMonogrammedSteeringWheelCover" onChange={setProductOption.bind(null, 'monogram')}
-                      pattern='[A-z]{3}'/>
+
+                    <div>{monogram}</div>
 
                     </div>
 
