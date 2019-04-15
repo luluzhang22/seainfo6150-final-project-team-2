0532ec31e92091f1e639813bebc02443e1dd6560
JustinCheng0509 <Yusi.Cheng.0509@Gmail.com>
2019-04-15 13:36:04 -0700

    order step 2 update

--- src/Order/OrderStep2.js
+++ src/Order/OrderStep2.js
@@ -34,54 +34,51 @@ class OrderStep2 extends Component {
         <div>
           <OrderTabs cur={2} selectedOptions={selectedOptions} product={product}
             productImg={selectedProductImg} />
-
-          <div className={styles.exhausts}>
-            <form onSubmit={this.handleSubmit.bind(this)}>
-              <div className={styles.orderStep2Options}>
-                <div>
-                  <select id="exhaust-select" onChange={setProductOption.bind(null, 'numExhausts')}>
-                    {options.numExhausts.name}:
+          <form onSubmit={this.handleSubmit.bind(this)}>
+          <div className={styles.orderStep2Options}>
+            <div>
+              <select id="exhaust-select" onChange={setProductOption.bind(null, 'numExhausts')}>
+                {options.numExhausts.name}:
                     <option value="1">Number of exhausts</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                  </select>
-                </div>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+              </select>
+            </div>
 
-                <div className={styles.orderStep2Options}>
-                  <a>Select Your Car Color</a>
-                  <input type="color" onChange={setProductOption.bind(null, 'color')} />
-                </div>
+            <div>
+              <a>Select Your Car Color</a>
+              <input type="color" onChange={setProductOption.bind(null, 'color')} />
+            </div>
 
-                <div>
-                  <select id="tintedWindows" onChange={setProductOption.bind(null, 'hasTintedWindows')}>
-                    <option value="No">Tinted Windows</option>
-                    <option value="No">No</option>
-                    <option value="Yes">Yes</option>
-                  </select>
-                </div>
+            <div>
+              <select id="tintedWindows" onChange={setProductOption.bind(null, 'hasTintedWindows')}>
+                <option value="No">Tinted Windows</option>
+                <option value="No">No</option>
+                <option value="Yes">Yes</option>
+              </select>
+            </div>
 
-                <div>
-                  <select id="hubcapMaterials" onChange={setProductOption.bind(null, 'hubcapsMaterial')}>
-                    <option>Hubcap Materials</option>
-                    <option value="chrome">Chrome</option>
-                    <option value="steel">Steel</option>
-                    <option value="plastic">Plastic</option>
-                  </select>
-                </div>
-
-              </div>
-              <div className={styles.orderFooter}>
-                <Link to='/order/1'>
-                  <input type="button" value="Previous"/>
-                </Link>
-                  <div>
-                <input type="submit" value="Next" />
-                  </div>
-              </div>
-            </form>
+            <div>
+              <select id="hubcapMaterials" onChange={setProductOption.bind(null, 'hubcapsMaterial')}>
+                <option>Hubcap Materials</option>
+                <option value="chrome">Chrome</option>
+                <option value="steel">Steel</option>
+                <option value="plastic">Plastic</option>
+              </select>
+            </div>
           </div>
+
+          <div className={styles.orderFooter}>
+              <Link to='/order/1'>
+                <input type="button" value="Previous" />
+              </Link>
+              <div>
+                <input type="submit" value="Next" />
+              </div>
+          </div>
+          </form>
         </div>
       )
   }
